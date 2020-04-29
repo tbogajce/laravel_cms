0ab867e45f4466a59ba497bceb5c8774b76d4374
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-04-29 11:23:55 +0200

    Added delete user and middleware for admin page

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -135,6 +135,8 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::findOrFail($id)->delete();
+
+        return redirect('/admin/users');
     }
 }
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -49,6 +49,14 @@
 
             {!! Form::close() !!}
 
+            {!! Form::open(['method'=>'DELETE', 'action'=>['AdminUsersController@destroy', $user->id]]) !!}
+
+            <div class="form-group">
+                {!! Form::submit('Delete User', ['class'=>'btn btn-danger']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
         </div>
     </div>
 
